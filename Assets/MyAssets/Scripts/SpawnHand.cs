0ce138c00cda16a39e8bfd45fadc028e46016965
JamieCCH <jamie.cch@gmail.com>
2017-08-26 13:51:31 -0400

    merge

--- Assets/MyAssets/Scripts/SpawnHand.cs
+++ Assets/MyAssets/Scripts/SpawnHand.cs
@@ -6,24 +6,25 @@ public class SpawnHand : MonoBehaviour {
 
 	//[SerializeField] Vector2 dest;
 	//[SerializeField] float interval;
-	[SerializeField] GameObject hand;
+	[SerializeField] GameObject[] hand;
+	public Transform upperLeft;
+	public Transform lowerRight;
+
 
 	bool canSpawn = true;
 
-	public float MinX = -6.05f;
-	public float MaxX = 7.25f;
-	public float MinY = -1.75f;
-	public float MaxY = 1.95f;
 
-	void Star(){
+	void Update(){
 		SpawnObj ();
 	}
 
 	void SpawnObj(){
-		float x = Random.Range(MinX,MaxX);
-		float y = Random.Range(MinY,MaxY);
+		int index = Random.Range (0, hand.Length - 1);
+
+		float x = Random.Range(upperLeft.position.x,lowerRight.position.x);
+		float y = Random.Range(upperLeft.position.y,lowerRight.position.y);
 		if (canSpawn) {
-			Instantiate(hand.gameObject, new Vector3(x,y,0), transform.rotation);
+			Instantiate(hand[index], new Vector3(x,y,0), transform.rotation);
 		}
 	}
 
