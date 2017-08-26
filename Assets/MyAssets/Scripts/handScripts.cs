44a56439200bb23c7b0dc77ef9d360133d3675ca
JamieCCH <jamie.cch@gmail.com>
2017-08-26 14:30:35 -0400

    hands

--- Assets/MyAssets/Scripts/handScripts.cs
+++ Assets/MyAssets/Scripts/handScripts.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class handScripts : MonoBehaviour {
 
+	public float speed = 2;
+	public Transform hand;
+	public Transform endPos;
+	public Transform startPos;
+	Transform currentPos;
+
+	void OnTriggerEnter2D(Collider2D other){
+	
+		if (other.tag == "projectile") {
+			GameManager.instance.RemoveHand ();
+			Destroy (other.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		currentPos = endPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if (currentPos == endPos) {
+			if (hand.position.y > endPos.position.y) {
+				hand.transform.Translate (Vector3.down * speed * Time.deltaTime);
+			} else {
+				currentPos = startPos;
+			}
+		} else {
+			if (hand.position.y < startPos.position.y) {
+				hand.transform.Translate (Vector3.up * speed * Time.deltaTime);
+			} else {
+				currentPos = endPos;
+			}
+		}
+			
 	}
 }
