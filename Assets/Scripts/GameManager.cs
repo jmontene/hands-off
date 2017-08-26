4471464b26273bd39ba2a5a507d6f5f73fa4c12f
Jose Montenegro <josmontenegro2012@gmail.com>
2017-08-26 10:36:51 -0400

    Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	[Header("Stats")]
+	[SerializeField] int tolerance = 100;
+
+	// Use this for initialization
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
