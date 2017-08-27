0196ea9a84511f7060160508b81dc1c9d4d0dea4
Jose Montenegro <josmontenegro2012@gmail.com>
2017-08-26 20:07:58 -0400

    build

--- Assets/MyAssets/Scripts/Projectile.cs
+++ Assets/MyAssets/Scripts/Projectile.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
 	[SerializeField] float speed;
+	[SerializeField] float aliveTime = 2f;
 	[HideInInspector] public Vector2 direction;
-	
+
+	void Start(){
+		Destroy(this.gameObject, aliveTime);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (direction * speed * Time.deltaTime);
